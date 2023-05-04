0ec54203aee877af679c94174fca924a23293eef
Sunhye <49827929+mocha-kim@users.noreply.github.com>
2023-05-04 19:08:29 +0900

    [FEAT] : skill struct 및 effectSkill 구성

--- Assets/Scripts/Actor/ActorStatObject.cs
+++ Assets/Scripts/Actor/ActorStatObject.cs
@@ -3,12 +3,25 @@ using UnityEngine;
 
 namespace Actor
 {
+    public enum ElementalType
+    {
+        Default,
+        Fire,
+        Ice,
+        Wind,
+        Ground,
+        Light,
+        Dark, 
+        Buf,
+        Debuf
+    }
     [CreateAssetMenu(fileName = "Actor Data", menuName = "Scriptable Object/Actor Data")]
     public class ActorStatObject : ScriptableObject
     {
         public int health;
         public int atkPower;
         public int speed;
+        public ElementalType element;
     }
 }
 
