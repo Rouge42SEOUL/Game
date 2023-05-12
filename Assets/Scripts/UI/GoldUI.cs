492c03287c34c106a27552ab68d98c6f54352d89
gyyu <yugd1211@gmail.com>
2023-05-12 16:47:22 +0900

    FEAT: create Gold setting Method

---
+++ Assets/Scripts/UI/GoldUI.cs
@@ -0,0 +1,29 @@
+using TMPro;
+using UnityEngine;
+
+public class GoldUI : MonoBehaviour
+{
+    [SerializeField] private int gold;
+    public int Gold
+    {
+        get => gold;
+        set
+        {
+            gold = value;
+            SetGold();
+        }
+    }
+    private TextMeshProUGUI _TMP;
+    
+    private void Start()
+    {
+        _TMP = transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+        SetGold();
+    }
+
+    public void SetGold()
+    {
+        Debug.Log("set");
+        _TMP.text = gold.ToString();
+    }
+}
