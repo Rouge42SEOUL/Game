294d172bb97bf2b09fa2077badefc8fcccb07814
Sunhye <49827929+mocha-kim@users.noreply.github.com>
2023-05-24 14:41:13 +0900

    ENV: managers replace

---
+++ Assets/Scripts/Managers/DataManager/DataContainer.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Managers.DataManager
+{
+    [JsonObject(MemberSerialization.OptIn)]
+    public class DataContainer
+    {
+        [JsonProperty]
+        public int Map;
+        [JsonProperty] 
+        public Dictionary<int, EventType> Events;
+        [JsonProperty]
+        public int PlayerCurrentNode;
+        [JsonProperty]
+        public int Gold;
+        [JsonProperty]
+        public bool IsEventRunning;
+
+        public void SaveInfo(Node[] nodes, Node currentNode)
+        {
+            Events = new Dictionary<int, EventType>();
+            for (int i = 0; i < nodes.Length; i++)
+            {
+                Events.Add(i, nodes[i].eventType);
+                if (nodes[i] == currentNode)
+                {
+                    PlayerCurrentNode = i;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Managers/DataManager/SaveNLoad.cs
@@ -0,0 +1,7 @@
+namespace Managers.DataManager
+{
+    public class SaveNLoad
+    {
+        
+    }
+}
